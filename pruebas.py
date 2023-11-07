08bbe9bdeec8fdfff683fa87ae7c38cacdc0013c
Skylethx <144559210+Skylethx@users.noreply.github.com>
2023-11-07 12:31:35 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -12,6 +12,9 @@ def normalize(s):
         s = s.replace(a, b).replace(a.upper(), b.upper())
     return s
 
+def identificar_abecedario(a):
+    español,frances,aleman,portugues=[ñ,á,é,í,ó,ú,ü,ý],[æ,œ,ç,à,â,é,è,ê,ë,î,ï,ô,ù,û,ü,ÿ],[ä,ö,ü,ß],[á,à,â,ã,é,ê,í,ó,ô,õ,ú]
+    pass
 
 print(normalize("¡Hólá, múndó!"))
 print(normalize("¡HÓLÁ, MÚNDÓ!"))
